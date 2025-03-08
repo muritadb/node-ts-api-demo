55d60b2e5d48c3aada3a04b6af5cb2f0e9bcab36
Douglas Murita <douglasb008@gmail.com>
2025-03-08 16:53:24 -0300

    fix: delete files unnecessary

--- template-pure-ts/src/index.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('validate mocked data', () => {
-  it('should have a name', () => {
-    expect(1).toBe(1)
-  })
-})
--- template-pure-ts/src/index.ts
+++
@@ -1,2 +0,0 @@
-import { mockedData } from '@/tests/data'
-console.log('Hello World!', mockedData.name)
